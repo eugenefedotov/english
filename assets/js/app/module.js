9d04c050f9d186b4274dee31aed9f257344030f0
FENIX <fenix123fly@gmail.com>
2015-05-26 23:42:10 +0300

    upd

--- assets/js/app/module.js
+++ assets/js/app/module.js
@@ -2,84 +2,38 @@
     'use strict';
 
     angular.module('English', [
-        'ngRoute'
-    ]).config(EnglishConfig);
+        'ngRoute', 'ngSanitize'
+    ])
+        .config(EnglishConfig)
+        .filter('safeHtml', function ($sce) {
+            return function(value) {
+                return $sce.trustAsHtml(value);
+            };
+        });
 
     EnglishConfig.$inject = ['$routeProvider'];
     function EnglishConfig($routeProvider) {
-        console.log($routeProvider);
         $routeProvider
             .when('/', {
                 templateUrl : 'view/main.html',
                 controller  : 'MainController',
                 title       : 'Welcome to Home Page'
             })
-            .when('/article', {
-                templateUrl : 'view/articles.html',
-                controller  : 'ThemeController'
+            .when('/rules/:rule_id', {
+                templateUrl : 'view/rules.html',
+                controller  : 'RuleController'
             })
-            .when('/noun', {
-                templateUrl : 'view/noun.html',
-                controller  : 'ThemeController'
-            })
-            .when('/pronouns', {
-                templateUrl : 'view/pronouns.html',
-                controller  : 'ThemeController'
-            })
-            .when('/adjectives-degrees-of-comparison', {
-                templateUrl : 'view/adjectives-degrees-of-comparison.html',
-                controller  : 'ThemeController'
-            })
-            .when('/prepositions', {
-                templateUrl : 'view/prepositions.html',
-                controller  : 'ThemeController'
-            })
-            .when('/usage-of-tenses', {
-                templateUrl : 'view/usage_of_tenses.html',
-                controller  : 'ThemeController'
-            })
-            .when('/passive-voice', {
-                templateUrl : 'view/passive_voice.html',
-                controller  : 'ThemeController'
-            })
-            .when('/sequence-of-tenses', {
-                templateUrl : 'view/sequence_of_tenses.html',
-                controller  : 'ThemeController'
-            })
-            .when('/infinitive', {
-                templateUrl : 'view/infinitive.html',
-                controller  : 'ThemeController'
-            })
-            .when('/communion', {
-                templateUrl : 'view/communion.html',
-                controller  : 'ThemeController'
-            })
-            .when('/gerund', {
-                templateUrl : 'view/gerund.html',
-                controller  : 'ThemeController'
-            })
-            .when('/communion-gerund-verbalnoun', {
-                templateUrl : 'view/communion_gerund_verbalnoun.html',
-                controller  : 'ThemeController'
-            })
-            .when('/complex-object', {
-                templateUrl : 'view/complex_object.html',
-                controller  : 'ThemeController'
-            })
-            .when('/complex-subject', {
-                templateUrl : 'view/complex_subject.html',
-                controller  : 'ThemeController'
-            })
-            .when('/conditional-sentences', {
-                templateUrl : 'view/conditional_sentences.html',
-                controller  : 'ThemeController'
-            })
-            .when('/modal-verbs', {
-                templateUrl : 'view/modal_verbs.html',
-                controller  : 'ThemeController'
+            .when('/articles', {
+                templateUrl : 'view/articles.html'
             })
             .otherwise({
                 redirectTo : '/'
             });
     }
 })();
+
+jQuery(document).ready(function($) {
+    $(document).tooltip({
+        selector: "attr[data-type='tooltip']"
+    });
+});
