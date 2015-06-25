69c80cc17e50f4b8c6e875d3486d6a08dc207348
FENIX <fenix123fly@gmail.com>
2015-06-25 22:29:05 +0300

    add themes and rules in adminpanel

--- config/adminpanel.js
+++ config/adminpanel.js
@@ -3,34 +3,64 @@
 module.exports.adminpanel = {
     instances : {
 
-        users : {
+        /* users : {
 
-            title : 'Users',
-            model : 'User',
+         title : 'Users',
+         model : 'User',
+
+         list : {
+         fields : {
+         id        : 'ID',
+         email     : 'Email',
+         active    : 'Active',
+         admin     : 'Admin',
+         createdAt : 'Created'
+         }
+         },
+
+         edit : {
+         fields : {
+         email : 'Email',
+
+         active : {
+         title : 'Active'
+         },
+         admin  : {
+         title    : 'Admin',
+         disabled : true
+         }
+         }
+         }
+         },*/
+        themes : {
+            title : 'Themes',
+            model : 'Themes',
 
             list : {
                 fields : {
-                    id        : 'ID',
-                    email     : 'Email',
-                    active    : 'Active',
-                    admin     : 'Admin',
-                    createdAt : 'Created'
-                }
-            },
-
-            edit : {
-                fields : {
-                    email : 'Email',
-
-                    active : {
-                        title : 'Active'
-                    },
-                    admin  : {
-                        title    : 'Admin',
-                        disabled : true
-                    }
+                    id   : 'ID',
+                    name : 'Name',
+                    link : 'Link',
+                    createdAt: false,
+                    updatedAt: false
                 }
             }
-        }
+        },
+        rules : {
+            title : 'Rules',
+            model : 'Rules',
+
+            list : {
+                fields : {
+                    text     : 'Text',
+                    status   : 'Status',
+                    theme_id : 'Theme ID',
+                    id       : 'ID',
+                    createdAt: false,
+                    updatedAt: false
+                }
+            }
+        },
+
     }
 };
